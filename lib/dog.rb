b878ce14c495bfca2a1ccf3b946bf9fc989fe025
jelizasilatei <silateijc90@gmail.com>
2023-05-19 22:58:14 +0300

    getters and setters

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
+    # setter
+    def name=(dog_name)
+        @name = dog_name
+    end
 
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+
+    # getter
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
+
+sushie = Dog.new
+sushie.name = "Sushie"
+puts sushie.name
+
+chiwawa = Dog.new
+chiwawa.breed = "Chiwawa"
+puts chiwawa.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
+    # setter
+    def name=(person_name)
+        @name = person_name
+    end
+
+    def job=(person_job)
+        @job = person_job
+    end
+
+    # getter
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+jeliza = Person.new
+jeliza.name = "Jeliza"
+puts jeliza.name
+
+cook = Person.new
+cook.job = "Cook"
+puts cook.job
